5f5eccbd25b793e0987a85eacc7d9ef67be7d28f
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-08-31 16:33:47 +0200

    add redux saga to auth, burger builder and orders

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import registerServiceWorker from './registerServiceWorker'
 import burgerBuilderReducer from './store/reducers/burgerBuilder'
 import orderReducer from './store/reducers/order'
 import authReducer from './store/reducers/auth'
+import createSagaMiddleware from 'redux-saga'
+import { watchAuth, watchBurgerBuilder, watchOrder } from './store/sagas'
 
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose
@@ -20,9 +22,15 @@ const rootReducer = combineReducers({
     auth: authReducer
 })
 
+const sagaMiddleware= createSagaMiddleware()
+
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, sagaMiddleware)
 ))
+
+sagaMiddleware.run(watchAuth)
+sagaMiddleware.run(watchBurgerBuilder)
+sagaMiddleware.run(watchOrder)
 
 const app = (
     <Provider store={store}>
